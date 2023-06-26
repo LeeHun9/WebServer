161cc15d855bb0e41cf15d686f0e2a8233146345
LeeHun9 <08173076@cumt.edu.cn>
2023-06-26 21:38:45 +0800

    version1.0 first commit

--- verison1.0/locker.h
+++ verison1.0/locker.h
@@ -5,14 +5,17 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-// 互斥锁
+// 线程同步机制封装类
+
+// 互斥锁类
 class locker {
 public:
     locker() {
-        if (pthread_mutex_init(&m_mutex, NULL) != 0) {
+        if(pthread_mutex_init(&m_mutex, NULL) != 0) {
             throw std::exception();
         }
     }
+
     ~locker() {
         pthread_mutex_destroy(&m_mutex);
     }
@@ -25,7 +28,8 @@ public:
         return pthread_mutex_unlock(&m_mutex) == 0;
     }
 
-    pthread_mutex_t* get() {
+    pthread_mutex_t *get()
+    {
         return &m_mutex;
     }
 
@@ -33,75 +37,67 @@ private:
     pthread_mutex_t m_mutex;
 };
 
-// 条件变量
+
+// 条件变量类
 class cond {
 public:
-    cond() {
+    cond(){
         if (pthread_cond_init(&m_cond, NULL) != 0) {
             throw std::exception();
         }
     }
     ~cond() {
-        if (pthread_cond_destroy(&m_cond, NULL) != 0) {
-            throw std::exception();
-        }
+        pthread_cond_destroy(&m_cond);
     }
 
-    bool wait(pthread_mutex_t* m_mutex) {
-        int ret;
+    bool wait(pthread_mutex_t *m_mutex) {
+        int ret = 0;
         ret = pthread_cond_wait(&m_cond, m_mutex);
         return ret == 0;
     }
-
-    bool timedwait(pthread_mutex_t* m_mutex, timespec* time) {
-        int ret;
-        ret = pthread_cond_timedwait(&m_cond, m_mutex, time);
+    bool timewait(pthread_mutex_t *m_mutex, struct timespec t) {
+        int ret = 0;
+        ret = pthread_cond_timedwait(&m_cond, m_mutex, &t);
         return ret == 0;
     }
-
     bool signal() {
         return pthread_cond_signal(&m_cond) == 0;
     }
-
     bool broadcast() {
         return pthread_cond_broadcast(&m_cond) == 0;
     }
+
 private:
     pthread_cond_t m_cond;
 };
 
-// 信号量
+
+// 信号量类
 class sem {
 public:
     sem() {
-        if( sem_init(&m_sem, 0, 0) != 0) {
+        if( sem_init( &m_sem, 0, 0 ) != 0 ) {
             throw std::exception();
         }
     }
-
     sem(int num) {
-        if( sem_init(&m_sem, 0, num) != 0) {
+        if( sem_init( &m_sem, 0, num ) != 0 ) {
             throw std::exception();
         }
     }
-
     ~sem() {
-        sem_destroy(&m_sem);
+        sem_destroy( &m_sem );
     }
-
     // 等待信号量
     bool wait() {
-        return sem_wait(&m_sem) == 0;
+        return sem_wait( &m_sem ) == 0;
     }
     // 增加信号量
     bool post() {
-        return sem_post(&m_sem) == 0;
+        return sem_post( &m_sem ) == 0;
     }
-
 private:
     sem_t m_sem;
 };
-
-
 
 #endif
